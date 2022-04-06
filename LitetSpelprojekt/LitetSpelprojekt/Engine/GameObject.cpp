606bc0c8c5e490f6e20af3bf04053106bd065fff
MattinJ <mattin.johansson@live.se>
2022-04-06 13:16:37 +0200

    Added name and tag on GameObject
    Get and set function and an additional constructor.

--- LitetSpelprojekt/LitetSpelprojekt/Engine/GameObject.cpp
+++ LitetSpelprojekt/LitetSpelprojekt/Engine/GameObject.cpp
@@ -8,6 +8,32 @@ GameObject::GameObject(ECS& ecs)
 	this->addComponent<Transform>();
 }
 
+GameObject::GameObject(ECS& ecs, std::string name, std::string tag)
+	:ecs(ecs), name(name), tag(tag)
+{
+	this->addComponent<Transform>();
+}
+
 GameObject::~GameObject()
 {
+}
+
+void GameObject::setName(std::string name)
+{
+	this->name = name;
+}
+
+void GameObject::setTag(std::string tag)
+{
+	this->tag = tag;
+}
+
+std::string GameObject::getName() const
+{
+	return this->name;
+}
+
+std::string GameObject::getTag() const
+{
+	return this->tag;
 }
--- LitetSpelprojekt/LitetSpelprojekt/Engine/GameObject.h
+++ LitetSpelprojekt/LitetSpelprojekt/Engine/GameObject.h
@@ -6,10 +6,19 @@ class GameObject
 {
 private:
 	ECS& ecs;
+	std::string name;
+	std::string tag;
+
 
 public:
 	GameObject(ECS& ecs);
+	GameObject(ECS& ecs, std::string name, std::string tag);
 	~GameObject();
+
+	void setName(std::string name);
+	void setTag(std::string tag);
+	std::string getName() const;
+	std::string getTag() const;
 
 	template <typename T>
 	void addComponent();
