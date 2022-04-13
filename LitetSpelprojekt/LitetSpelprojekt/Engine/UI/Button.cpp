94385b5d23de1fcdce19c6f1a1d746e2516a398d
Joel Berg <joel.berg@hotmail.se>
2022-04-13 15:19:42 +0200

    MenuButtonClass
    Added class for menu buttons

--- LitetSpelprojekt/LitetSpelprojekt/Engine/UI/Button.cpp
+++ LitetSpelprojekt/LitetSpelprojekt/Engine/UI/Button.cpp
@@ -2,19 +2,40 @@
 
 Button::Button()
 {
-	this->width = 0;
-	this->height = 0;
+	this->w = 0;
+	this->h = 0;
+	this->pos[0] = 0;
+	this->pos[1] = 0;
+}
+
+Button::Button(int w = 0, int h = 0, int[] p)
+{
+	this->width = w;
+	this->height = h;
+	this->pos[0] = p[0];
+	this->pos[1] = p[1];
 }
 
 Button::~Button()
 {
-
 }
 
 bool Button::isClicked()
 {
 	bool buttonClicked = false;
 
+	int maxPosX = pos[0] + (width / 2);
+	int minPosX = pos[0] - (width / 2);
+	int maxPosY = pos[1] + (height / 2);
+	int minPosY = pos[1] + (height / 2);
+
+	if (Input::getCursorX() >= minPosX && (Input::getCursorX() <= maxPosX))
+	{
+		if (Input::getCursorY() >= minPosY && Input::getCursorY() <= maxPosY)
+		{
+			buttonClicked = true;
+		}
+	}
 
 	return buttonClicked;
 }
--- LitetSpelprojekt/LitetSpelprojekt/Engine/UI/Button.h
+++ LitetSpelprojekt/LitetSpelprojekt/Engine/UI/Button.h
@@ -1,16 +1,19 @@
 #pragma once
+#include "../Application/Input.h"
 
 class Button
 {
 private:
 	int width;
 	int height;
-
+	int pos[2];
 public:
 	Button();
+	Button(int w, int h, int[] pos);
 	~Button();
 
 	inline int getWidth() { return this->width; }
 	inline int getheight() { return this->height; }
+	inline int getPosition() { return this->pos; }
 	bool isClicked();
 };
