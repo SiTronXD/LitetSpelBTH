39a15bde577e0f97b24893818356d65913e8cbac
Joel Berg <joel.berg@hotmail.se>
2022-04-14 16:01:41 +0200

    DisplayMenuScene
    Added buttons to the menu scene and displayed them through UIRenderer

--- LitetSpelprojekt/LitetSpelprojekt/Engine/Engine.cpp
+++ LitetSpelprojekt/LitetSpelprojekt/Engine/Engine.cpp
@@ -3,6 +3,7 @@
 #include "Time.h"
 #include "ResTranslator.h"
 #include "../ProjectSpecifics/Scenes/GameScene.h"
+#include "../ProjectSpecifics/Scenes/MenuScene.h"
 
 // Temp
 #include "ECS.h"
@@ -14,7 +15,7 @@
 Engine::Engine()
 	: renderer(this->resources),
 	uiRenderer(this->renderer, this->resources),
-	sceneHandler(this->resources, this->renderer)
+	sceneHandler(this->resources, this->renderer, this->uiRenderer)
 {
 	this->settings.loadSettings();
 	this->window.init(this->settings.getSettings().resolutionX, this->settings.getSettings().resolutionY, "Litet Spelprojekt");
@@ -22,7 +23,8 @@ Engine::Engine()
 	this->resources.init(&this->renderer);
 	this->uiRenderer.init(this->settings.getSettings().resolutionX, this->settings.getSettings().resolutionY);
 
-	this->sceneHandler.setScene(new GameScene(this->sceneHandler));
+	//this->sceneHandler.setScene(new GameScene(this->sceneHandler));
+	this->sceneHandler.setScene(new MenuScene(this->sceneHandler));
 }
 
 Engine::~Engine()
@@ -54,6 +56,7 @@ void Engine::run()
 
 		// Render UI
 		this->sceneHandler.getScene()->renderUI();
+		/*
 		this->uiRenderer.renderTexture(
 			"me.png", 
 			1920 / 2 - 100 / 2,
@@ -61,6 +64,8 @@ void Engine::run()
 			100,
 			100
 		);
+		*/
+		this->sceneHandler.getScene()->renderUI();
 
 		// Present
 		this->renderer.presentSC();
--- LitetSpelprojekt/LitetSpelprojekt/Engine/UI/Button.cpp
+++ LitetSpelprojekt/LitetSpelprojekt/Engine/UI/Button.cpp
@@ -1,14 +1,8 @@
 #include "Button.h"
+#include "../Dev/Log.h"
 
-Button::Button()
-{
-	this->posX = 0;
-	this->posY = 0;
-	this->width = 0;
-	this->height = 0;
-}
 
-Button::Button(int pX, int pY, int w, int h)
+Button::Button(int pX, int pY, int w, int h, UIRenderer& r) : uiRenderer(r)
 {
 	this->posX = pX;
 	this->posY = pY;
@@ -37,6 +31,10 @@ bool Button::isClicked()
 			buttonClicked = true;
 		}
 	}
-
 	return buttonClicked;
+}
+
+void Button::render(std::string textureName)
+{
+	uiRenderer.renderTexture(textureName, this->posX, this->posY, this->width, this->height);
 }
