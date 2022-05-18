0bd72d986eca719a3f0a192971026bb790da9489
Bensson <47039664+SiTronXD@users.noreply.github.com>
2022-05-18 20:02:32 +0200

    William's hand fix

--- LitetSpelprojekt/LitetSpelprojekt/ProjectSpecifics/Scenes/GameScene.cpp
+++ LitetSpelprojekt/LitetSpelprojekt/ProjectSpecifics/Scenes/GameScene.cpp
@@ -342,7 +342,7 @@ void GameScene::init()
 	);
 
 	MeshData handMeshData = MeshLoader::loadAnimatedModel(
-		"Resources/Models/handRig_02_edit.fbx");
+		"Resources/Models/handRig_02_edit2.fbx");
 	handMeshData.invertFaces();
 	this->getResources().addMesh(
 		std::move(handMeshData),
